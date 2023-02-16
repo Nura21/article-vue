616960c8219ece476fb8afd4c1020a00d1dac155
Faisal-88 <faisaltimbangan32@gmail.com>
2023-02-17 03:17:51 +0700

    commit pertama

---
+++ src/views/AboutView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="about">
+    <div class="container">
+        <h3 class="p-3 text-center">Vue.js - List Table</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Pengguna</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in users" :key="user.id">
+                    <td>{{user.firstName}} {{user.lastName}}</td>
+                    <td>{{user.email}}</td>
+                    <td>{{user.role}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  </div>
+  
+</template>
+
+<script scope>
+export default {
+    data() {
+        return {
+            users: [
+                { firstName: 'Frank', lastName: 'Murphy', email: 'frank.murphy@test.com', role: 'User' },
+                { firstName: 'Vic', lastName: 'Reynolds', email: 'vic.reynolds@test.com', role: 'Admin' },
+                { firstName: 'Gina', lastName: 'Jabowski', email: 'gina.jabowski@test.com', role: 'Admin' },
+                { firstName: 'Jessi', lastName: 'Glaser', email: 'jessi.glaser@test.com', role: 'User' },
+                { firstName: 'Jay', lastName: 'Bilzerian', email: 'jay.bilzerian@test.com', role: 'User' }
+            ]
+        };
+    }
+};
+</script>
+
+<!-- <style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style> -->
